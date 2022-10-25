00dcafffd20a7daec122311448bf267a8c57f2e3
Marc Devaud <marc.devaud@epsi.fr>
2022-10-25 10:38:06 +0200

    lecture csv

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
+from codecs import latin_1_decode
 import sys 
 import csv
 
 print (sys.version)
 print(sys.executable)
 
+#test
 
-with open("./conso-annuelles_v1.csv", 'r') as file:
+with open("./conso-annuelles_v1.csv", 'r',encoding='latin-1') as file:
   csvreader = csv.reader(file, delimiter=':')
   for row in csvreader:
     print(row)
